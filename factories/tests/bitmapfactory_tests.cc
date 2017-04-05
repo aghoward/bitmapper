eced6b17e98b49b362464def086b5f64faeb1006
howard <ahoward@foxguardsolutions.com>
2017-04-05 12:16:01 -0400

    Correct Makefile and add tests
    Adds another test and cleans up some existing test. Fixes makefile for
    those tests.

--- factories/tests/bitmapfactory_tests.cc
+++ factories/tests/bitmapfactory_tests.cc
@@ -12,6 +12,7 @@
 using ::testing::_;
 using ::testing::Return;
 using ::testing::Exactly;
+using ::testing::NiceMock;
 
 #include <iostream>
 
@@ -20,9 +21,9 @@ using namespace std;
 class BitmapFactoryTests : public ::testing::Test {
     protected:
         BitmapFactory _subject;
-        MockBitmapHeaderFactory _mockBitmapHeaderFactory;
-        MockBitmapCoreHeaderFactory _mockBitmapCoreHeaderFactory;
-        MockColorTableHeaderFactory _mockColorTableHeaderFactory;
+        NiceMock<MockBitmapHeaderFactory> _mockBitmapHeaderFactory;
+        NiceMock<MockBitmapCoreHeaderFactory> _mockBitmapCoreHeaderFactory;
+        NiceMock<MockColorTableHeaderFactory> _mockColorTableHeaderFactory;
 
         shared_ptr<BitmapHeader> _bitmapHeader;
         shared_ptr<BitmapCoreHeader> _bitmapCoreHeader;
@@ -53,26 +54,30 @@ TEST_F(BitmapFactoryTests, Create_CallsColorTableHeaderFactoryWithEvenColors) {
     auto expectedValue = bitsPerPixel / 3;
     auto pixels = string(expectedValue * 16, 0x0);
 
-    EXPECT_CALL(_mockBitmapHeaderFactory, Create(_, _))
-        .Times(Exactly(2));
-    EXPECT_CALL(_mockBitmapCoreHeaderFactory, Create(_, _))
-        .Times(Exactly(1));
     EXPECT_CALL(_mockColorTableHeaderFactory, Create(expectedValue, expectedValue, expectedValue))
         .Times(Exactly(1));
 
     auto result = _subject.Create(pixels, bitsPerPixel);
 }
 
+TEST_F(BitmapFactoryTests, Create_CallsColorTableHeaderFactoryWithUnevenColors) {
+    auto bitsPerPixel = 8;
+    auto expectedValue = bitsPerPixel / 3;
+    auto pixels = string(expectedValue * 16, 0x0);
+
+    EXPECT_CALL(_mockColorTableHeaderFactory, Create(expectedValue, expectedValue, expectedValue + 2))
+        .Times(Exactly(1));
+
+    auto result = _subject.Create(pixels, bitsPerPixel);
+}
+
+
 TEST_F(BitmapFactoryTests, Create_CallsBitmapCoreHeaderFactoryWithFilledPixelArray) {
     auto bitsPerPixel = 8;
     auto pixels = string(bitsPerPixel * 12, 0x1);
     auto expected = pixels + string(4, 0x0);
 
-    EXPECT_CALL(_mockBitmapHeaderFactory, Create(_, _))
-        .Times(Exactly(2));
     EXPECT_CALL(_mockBitmapCoreHeaderFactory, Create(expected, bitsPerPixel))
-        .Times(Exactly(1));
-    EXPECT_CALL(_mockColorTableHeaderFactory, Create(_, _, _))
         .Times(Exactly(1));
 
     auto result = _subject.Create(pixels, bitsPerPixel);
